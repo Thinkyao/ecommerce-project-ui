2fb411876abc8be71dac656ce8f71c517259da32
Thinkyao <lisiyao217@gmail.com>
2023-05-17 13:09:35 -0700

    custom validator rule

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { SiaShopFormService } from 'src/app/services/sia-shop-form.service';
+import { SiaShopValidators } from 'src/app/validators/sia-shop-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -31,10 +32,17 @@ export class CheckoutComponent implements OnInit {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        firstName: new FormControl('',
+                                  [Validators.required,
+                                  Validators.minLength(2),
+                                  SiaShopValidators.notOnlyWhitespace]),
+        lastName: new FormControl('',
+                                  [Validators.required,
+                                  Validators.minLength(2),
+                                  SiaShopValidators.notOnlyWhitespace]),
         email: new FormControl('',
-          [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
+                                  [Validators.required,
+                                  Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
---
+++ src/app/validators/sia-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { SiaShopValidators } from './sia-shop-validators';
+
+describe('SiaShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new SiaShopValidators()).toBeTruthy();
+  });
+});
