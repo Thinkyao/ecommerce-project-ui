47b9f2f2973052bdb20e8e99a12980c09a868558
Thinkyao <lisiyao217@gmail.com>
2023-05-23 22:15:48 -0700

    add Login/Logout function-okta

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa9okz2ehLahixUa5d7',
+        issuer: 'https://dev-97778387.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
